15604d8bba5e8f90a1b5b8e75d8a45d81598ea2d
emmimat <emmi.matila@gmail.com>
2023-03-25 18:48:47 +0200

    kertauksena 1.1 ja 1.2 tehty

---
+++ kertaus/kurssitiedot/src/App.js
@@ -0,0 +1,57 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+const Header = () => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>  
+  )
+}
+
+const Part = (props) => {
+  return (
+     <div>
+       <p>
+         {props.part} {props.exercises}
+       </p>
+//     </div>
+   )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+     <div>
+       <Part part={props.part1} exercises={props.exercises1}/>
+       <Part part={props.part2} exercises={props.exercises2}/>
+       <Part part={props.part3} exercises={props.exercises3}/>
+     </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>  
+  )
+}
+
+  return (
+    <div>
+       <Header course={course} />
+       <Content Part part1={part1} />
+       <Total exercises1={exercises1} />
+    </div>
+  )
+}
+
+export default App
